807475b45176365a1712cd0ec9524038824237cb
Zully Barrientos <zullybarrientos@example.com>
2018-08-15 17:26:54 -0700

    assignment step 1 for 5CP8

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -14,5 +14,7 @@
 
 {
 $('#album-title').text(album.title);
-$('img#album-cover-art').attr('src', album.albumArtUrl);  
+$('img#album-cover-art').attr('src', album.albumArtUrl);
+$('.artist').text(album.artist);
+$('#release-info').text(album.releaseInfo);
 }
