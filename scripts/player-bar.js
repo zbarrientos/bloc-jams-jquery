d03a831b00a9ac4fa6a93588ba049d097cd630cc
Zully Barrientos <zullybarrientos@example.com>
2018-08-29 12:06:44 -0700

    5CP11-checkpoint completed 1

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -28,17 +28,17 @@
     player.playPause(previousSong)
      });
 
-     $('#time-control input').on('input', function (event) {
-    player.skipTo(event.target.value);
-      });
+          $('#time-control input').on('input', function (event) {
+         player.skipTo(event.target.value);
+           });
 
-      setInterval( () => {
-    if (player.playState !== 'playing') { return; }
-    const currentTime = player.getTime();
-    const duration = player.getDuration();
-    const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text( currentTime );
-    $('#time-control input').val(percent);
-  }, 1000);
+           setInterval( () => {
+         if (player.playState !== 'playing') { return; }
+         const currentTime = player.getTime();
+         const duration = player.getDuration();
+         const percent = (currentTime / duration) * 100;
+         $('#time-control .current-time').text( currentTime );
+         $('#time-control input').val(percent);
+       }, 1000);
 
 }
