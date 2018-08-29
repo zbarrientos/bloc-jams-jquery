39f08d87329e22e0666675d5ee974be7d5c101c5
Zully Barrientos <zullybarrientos@example.com>
2018-08-28 19:22:46 -0700

    5CP10-assignment

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,6 +4,7 @@
     $(this).attr('playState', player.playState);
   });
 
+
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
@@ -15,4 +16,15 @@
     player.playPause(nextSong);
   });
 
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex <= -1) {  return; } 
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong)
+     });
 }
