16be3ad9dea76da19db2f687171dc9298638be36
Zully Barrientos <zullybarrientos@example.com>
2018-09-05 09:49:00 -0700

    5CP11-assignment, SLIDERS; 8th commit, still troubleshooting

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,7 +1,8 @@
 class Helper {
-  playPauseAndUpdate (song) {
-      player.playPause(song);
-      const duration = player.currentlyPlaying.duration;
-      $('#time-control .total-time').text(this.prettyTime(duration));
-    }
-  const helper = new Helper();
+  playPauseAndUpdate(song) {
+     player.playPause(song);
+     const duration = player.currentlyPlaying.duration;
+     $('#time-control .total-time').text(this.prettyTime(duration));
+     }
+ }
+   const helper = new Helper();
