776a26161752efb8491c8798e3737ccfde8abcbe
Zully Barrientos <zullybarrientos@example.com>
2018-09-05 16:27:28 -0700

    5CP11-assignment, 9th commit, BACKTRACKING and TROUBLESHOOTING

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,8 @@
-class Helper {
-  playPauseAndUpdate(song) {
-     player.playPause(song);
-     const duration = player.currentlyPlaying.duration;
-     $('#time-control .total-time').text(this.prettyTime(duration));
-     }
- }
-   const helper = new Helper();
+   class Helper {
+     playPauseAndUpdate(song) {
+       const duration = player.getDuration();
+       $('#time-control .total-time').text ( duration );
+       player.playPause(song);
+       }
+      }
+     const helper = new Helper();
